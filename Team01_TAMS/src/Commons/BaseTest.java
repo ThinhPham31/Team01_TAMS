b8fd517d6d6aa095f9e96a10c9bd30e90fc7cdb7
Em Tan 666 <Em Tan 666@DESKTOP-4SR0IR5>
2025-11-24 14:32:49 +0700

    a

---
+++ Team01_TAMS/src/Commons/BaseTest.java
@@ -0,0 +1,27 @@
+package Commons;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    // Gọi trong @BeforeTest của từng test class
+    protected void setupDriver() {
+        // Đường dẫn chromedriver.exe trong project
+        String driverPath = System.getProperty("user.dir")
+                + "\\resources\\drivers\\chromedriver.exe";
+
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
